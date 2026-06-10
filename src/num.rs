67573adf1fc1b8833909bed964398f95a02630a2
Relial <relialhyphen@gmail.com>
2026-06-10 23:10:38 +0300

    more slider methods

--- src/num.rs
+++ src/num.rs
@@ -1,40 +1,64 @@
+use serde::{Deserialize, Serialize};
+
 #[repr(C)]
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Serialize, Deserialize)]
 pub enum Num {
-    Integer(i32),
+    Integer(i64),
     Float(f64),
 }
 
 impl Num {
-    pub fn integer(integer: impl Into<i32>) -> Self {
+    pub fn new_integer(integer: impl Into<i64>) -> Self {
         Self::Integer(integer.into())
     }
 
-    pub fn float(float: impl Into<f64>) -> Self {
+    pub fn new_float(float: impl Into<f64>) -> Self {
         Self::Float(float.into())
+    }
+
+    pub const fn int(&self) -> bool {
+        matches!(self, Self::Integer(_))
+    }
+
+    pub const fn float(&self) -> bool {
+        matches!(self, Self::Float(_))
+    }
+
+    pub fn set(&mut self, value: f64) {
+        match self {
+            Num::Integer(int) => *int = value as i64,
+            Num::Float(float) => *float = value,
+        }
+    }
+
+    pub fn to_f64(self) -> f64 {
+        match self {
+            Num::Integer(int) => int as f64,
+            Num::Float(float) => float,
+        }
     }
 }
 
 impl From<Num> for f64 {
     fn from(value: Num) -> Self {
         match value {
-            Num::Integer(i) => i.into(),
+            Num::Integer(i) => i as f64,
             Num::Float(f) => f,
         }
     }
 }
 
-impl From<Num> for i32 {
+impl From<Num> for i64 {
     fn from(value: Num) -> Self {
         match value {
             Num::Integer(i) => i,
-            Num::Float(f) => f as i32,
+            Num::Float(f) => f as i64,
         }
     }
 }
 
-impl From<i32> for Num {
-    fn from(value: i32) -> Self {
+impl From<i64> for Num {
+    fn from(value: i64) -> Self {
         Self::Integer(value)
     }
 }
