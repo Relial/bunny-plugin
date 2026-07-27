26e31e9f3e2311edbc9dfba831bba1a12bf56d42
Relial <relialhyphen@gmail.com>
2026-07-27 07:29:22 +0300

    Fill out checkbox

--- src/widgets/checkbox.rs
+++ src/widgets/checkbox.rs
@@ -4,23 +4,44 @@ use crate::{elements::Widget, widget_text::WidgetText};
 
 #[repr(C)]
 pub struct CheckBox<'a> {
-    text: WidgetText,
-    value: &'a mut bool,
+    text: Option<WidgetText>,
+    checked: &'a mut bool,
+    indeterminate: bool,
 }
 
 impl<'a> CheckBox<'a> {
-    pub fn new(value: &'a mut bool, text: impl Into<WidgetText>) -> Self {
+    pub fn new(checked: &'a mut bool, text: impl Into<WidgetText>) -> Self {
         Self {
-            value,
-            text: text.into(),
+            text: Some(text.into()),
+            checked,
+            indeterminate: false,
         }
+    }
+
+    pub fn without_text(checked: &'a mut bool) -> Self {
+        Self {
+            text: None,
+            checked,
+            indeterminate: false,
+        }
+    }
+
+    #[inline]
+    pub fn indeterminate(mut self, indeterminate: bool) -> Self {
+        self.indeterminate = indeterminate;
+        self
     }
 }
 
 impl egui::Widget for CheckBox<'_> {
     fn ui(self, ui: &mut Ui) -> egui::Response {
-        let checked = self.value;
-        ui.checkbox(checked, self.text)
+        let mut checkbox = if let Some(text) = self.text {
+            egui::Checkbox::new(self.checked, text)
+        } else {
+            egui::Checkbox::without_text(self.checked)
+        };
+        checkbox = checkbox.indeterminate(self.indeterminate);
+        ui.add(checkbox)
     }
 }
 
