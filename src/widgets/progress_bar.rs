b5ac66e0a85cde848fb1c6f79b50d005327cec18
Relial <relialhyphen@gmail.com>
2026-07-28 16:12:26 +0300

    progress bar impl

--- src/widgets/progress_bar.rs
+++ src/widgets/progress_bar.rs
@@ -1,6 +1,6 @@
 use abi_stable::std_types::{
     RBox,
-    ROption::{self, RSome},
+    ROption::{self, RNone, RSome},
 };
 use egui::Color32;
 
@@ -23,7 +23,61 @@ pub struct ProgressBar {
     animate: bool,
 }
 
-impl ProgressBar {}
+impl ProgressBar {
+    pub fn new(progress: f32) -> Self {
+        Self {
+            text: RNone,
+            desired_width: RNone,
+            desired_height: RNone,
+            fill: RNone,
+            corner_radius: RNone,
+            progress,
+            animate: false,
+        }
+    }
+
+    #[inline]
+    pub fn desired_width(mut self, desired_width: f32) -> Self {
+        self.desired_width = RSome(desired_width);
+        self
+    }
+
+    #[inline]
+    pub fn desired_height(mut self, desired_height: f32) -> Self {
+        self.desired_height = RSome(desired_height);
+        self
+    }
+
+    #[inline]
+    pub fn fill(mut self, color: Color32) -> Self {
+        self.fill = RSome(color);
+        self
+    }
+
+    #[inline]
+    pub fn text(mut self, text: impl Into<WidgetText>) -> Self {
+        self.text = RSome(ProgressBarText::Custom(text.into()));
+        self
+    }
+
+    #[inline]
+    pub fn show_percentage(mut self) -> Self {
+        self.text = RSome(ProgressBarText::Percentage);
+        self
+    }
+
+    #[inline]
+    pub fn animate(mut self, animate: bool) -> Self {
+        self.animate = animate;
+        self
+    }
+
+    #[inline]
+    pub fn corner_radius(mut self, corner_radius: impl Into<CornerRadius>) -> Self {
+        self.corner_radius = RSome(corner_radius.into());
+        self
+    }
+}
 
 impl egui::Widget for ProgressBar {
     fn ui(self, ui: &mut egui::Ui) -> egui::Response {
