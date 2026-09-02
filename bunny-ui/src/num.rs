e19061bea5a4739af100398b12f808d7962f7acf
Relial <relialhyphen@gmail.com>
2026-09-02 05:45:08 +0300

    impl usize and isize for Num

--- bunny-ui/src/num.rs
+++ bunny-ui/src/num.rs
@@ -6,10 +6,12 @@ pub enum Num<'a> {
     U16(&'a mut u16),
     U32(&'a mut u32),
     U64(&'a mut u64),
+    USize(&'a mut usize),
     I8(&'a mut i8),
     I16(&'a mut i16),
     I32(&'a mut i32),
     I64(&'a mut i64),
+    ISize(&'a mut isize),
 }
 
 impl Num<'_> {
@@ -27,10 +29,12 @@ impl Num<'_> {
             Num::U16(u) => **u = value as u16,
             Num::U32(u) => **u = value as u32,
             Num::U64(u) => **u = value as u64,
+            Num::USize(u) => **u = value as usize,
             Num::I8(i) => **i = value as i8,
             Num::I16(i) => **i = value as i16,
             Num::I32(i) => **i = value as i32,
             Num::I64(i) => **i = value as i64,
+            Num::ISize(i) => **i = value as isize,
         }
     }
 
@@ -43,10 +47,12 @@ impl Num<'_> {
             Num::U16(u) => **u as f64,
             Num::U32(u) => **u as f64,
             Num::U64(u) => **u as f64,
+            Num::USize(u) => **u as f64,
             Num::I8(i) => **i as f64,
             Num::I16(i) => **i as f64,
             Num::I32(i) => **i as f64,
             Num::I64(i) => **i as f64,
+            Num::ISize(i) => **i as f64,
         }
     }
 
@@ -54,11 +60,12 @@ impl Num<'_> {
     pub fn min(&self) -> f64 {
         match self {
             Num::F32(_) | Num::F64(_) => f64::NEG_INFINITY,
-            Num::U8(_) | Num::U16(_) | Num::U32(_) | Num::U64(_) => 0.0,
+            Num::U8(_) | Num::U16(_) | Num::U32(_) | Num::U64(_) | Num::USize(_) => 0.0,
             Num::I8(_) => i8::MIN as f64,
             Num::I16(_) => i16::MIN as f64,
             Num::I32(_) => i32::MIN as f64,
             Num::I64(_) => i64::MIN as f64,
+            Num::ISize(_) => isize::MIN as f64,
         }
     }
 
@@ -70,10 +77,12 @@ impl Num<'_> {
             Num::U16(_) => u16::MAX as f64,
             Num::U32(_) => u32::MAX as f64,
             Num::U64(_) => u64::MAX as f64,
+            Num::USize(_) => usize::MAX as f64,
             Num::I8(_) => i8::MAX as f64,
             Num::I16(_) => i16::MAX as f64,
             Num::I32(_) => i32::MAX as f64,
             Num::I64(_) => i64::MAX as f64,
+            Num::ISize(_) => isize::MAX as f64,
         }
     }
 }
@@ -120,6 +129,13 @@ impl<'a> From<&'a mut u64> for Num<'a> {
     }
 }
 
+impl<'a> From<&'a mut usize> for Num<'a> {
+    #[inline]
+    fn from(value: &'a mut usize) -> Self {
+        Self::USize(value)
+    }
+}
+
 impl<'a> From<&'a mut i8> for Num<'a> {
     #[inline]
     fn from(value: &'a mut i8) -> Self {
@@ -145,5 +161,12 @@ impl<'a> From<&'a mut i64> for Num<'a> {
     #[inline]
     fn from(value: &'a mut i64) -> Self {
         Self::I64(value)
+    }
+}
+
+impl<'a> From<&'a mut isize> for Num<'a> {
+    #[inline]
+    fn from(value: &'a mut isize) -> Self {
+        Self::ISize(value)
     }
 }
