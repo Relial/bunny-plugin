e71b22a6b6a8c0021d19e6a7b8def8cf37df1295
Relial <relialhyphen@gmail.com>
2026-09-09 06:41:32 +0300

    No unload setting and some docs

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -20,15 +20,17 @@ use abi_stable::std_types::{
     ROption::{self, RNone, RSome},
     RString,
 };
-use anyhow::{Result, anyhow};
+#[cfg(feature = "manager")]
+use anyhow::Result;
+use anyhow::anyhow;
 use tracing_subscriber::filter::LevelFilter;
 
 use crate::hook::{Bunny3dCallback, Hooks, SimpleCallback, UiCallback};
 
+pub mod game_version;
 pub mod hook;
 pub mod hook_builder;
 pub mod hook_cell;
-pub mod game_version;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 #[repr(C)]
@@ -47,10 +49,13 @@ impl std::fmt::Display for GameMode {
     }
 }
 
+/// Information about the game dll (mhfo.dll/mhfo-hd.dll)
 #[derive(Clone, Copy, Debug)]
 #[repr(C)]
 pub struct MhfoInfo {
+    /// Whether the game's running in low grade (mhfo.dll) or high grade (mhfo-hd.dll) mode
     pub game_mode: GameMode,
+    /// The game dll's base address
     pub address: usize,
 }
 
@@ -77,16 +82,19 @@ impl PluginContext {
         }
     }
 
+    /// Information about the game dll (mhfo.dll/mhfo-hd.dll)
     #[inline]
     pub fn mhfo_info(&self) -> MhfoInfo {
         self.mhfo_info
     }
 
+    /// Path to bunny_config. Save plugin config and anything else you want to persist about the plugin state here.
     #[inline]
     pub fn config_dir(&self) -> &Path {
         Path::new(self.config_dir.as_str())
     }
 
+    /// Desired log level. This is configured by the user in cardamom-loader.toml
     #[inline]
     pub fn log_level(&self) -> LogLevel {
         self.log_level
@@ -100,6 +108,7 @@ pub struct PluginInfo {
     version: RString,
     pub hooks: Hooks,
     init_fail_reason: ROption<RString>,
+    pub no_unload: bool,
 }
 
 impl PluginInfo {
@@ -109,73 +118,95 @@ impl PluginInfo {
             version: version.into(),
             hooks: Default::default(),
             init_fail_reason: RNone,
+            no_unload: false,
         }
     }
 
+    /// Called early in the frame when not on a quest
     #[inline]
     pub fn lobby_hook(mut self, callback: SimpleCallback) -> Self {
         self.hooks.set_lobby(callback);
         self
     }
 
+    /// Called early in the frame during quests
     #[inline]
     pub fn quest_hook(mut self, callback: SimpleCallback) -> Self {
         self.hooks.set_quest(callback);
         self
     }
 
+    /// Called early in the frame during the quest end wait when a quest is complete, retired or failed
     #[inline]
     pub fn quest_ending_hook(mut self, callback: SimpleCallback) -> Self {
         self.hooks.set_quest_ending(callback);
         self
     }
 
+    /// Called early in the frame when a quest is complete, during the rewards screen and the fadeout
     #[inline]
     pub fn quest_complete_hook(mut self, callback: SimpleCallback) -> Self {
         self.hooks.set_quest_complete(callback);
         self
     }
 
+    /// Called once per frame when this plugin's manager dropdown menu is open, just before the frame is presented
     #[inline]
     pub fn ui_menu(mut self, callback: UiCallback) -> Self {
         self.hooks.set_ui_menu(callback);
         self
     }
 
+    /// Called once per frame just before the frame is presented
     #[inline]
     pub fn ui_free(mut self, callback: UiCallback) -> Self {
         self.hooks.set_ui_free(callback);
         self
     }
 
+    /// Called once per user defined autosave interval
     #[inline]
     pub fn save(mut self, callback: SimpleCallback) -> Self {
         self.hooks.set_save(callback);
         self
     }
 
+    /// Called once per frame at a timing that's suitable for 3d rendering
     #[inline]
     pub fn bunny3d(mut self, callback: Bunny3dCallback) -> Self {
         self.hooks.set_bunny3d(callback);
         self
     }
 
+    /// Specify a reason initialization failed. If you set this the manager won't make any more calls to the plugin, and the failure reason is shown to the user.
+    ///
+    /// The plugin will remain loaded unless toggled by the user
     #[inline]
     pub fn init_fail(mut self, fail_reason: impl Into<RString>) -> Self {
         self.init_fail_reason = RSome(fail_reason.into());
         self
     }
 
+    /// Specify that the plugin shouldn't be unloaded. The plugin's toggle checkmark will be faded and uninteractable.
+    #[inline]
+    pub fn no_unload(mut self) -> Self {
+        self.no_unload = true;
+        self
+    }
+
+    #[cfg(feature = "manager")]
     #[inline]
     pub fn name(&self) -> &str {
         &self.name
     }
 
+    #[cfg(feature = "manager")]
     #[inline]
     pub fn version(&self) -> &str {
         &self.version
     }
 
+    #[cfg(feature = "manager")]
     #[inline]
     pub fn init(&self) -> Result<()> {
         if let RSome(fail_reason) = &self.init_fail_reason {
